452d5199149bf8561f5b2b9cfc0a214056cd3e9c
tlsrb0025 <tlsrb0025@gmail.com>
2022-01-26 07:20:08 +0900

    페이징적용,이미지로딩효과적용,페이징버튼수정,리뷰수정취소버튼구현,카카오이미지앵커.

--- racket/migrations/0002_dummy.py
+++ racket/migrations/0002_dummy.py
@@ -4,22 +4,6 @@ from racket.models import Racket, RacketDetail
 
 
 def gen_master(apps, schema_editor):
-    Racket(name="블레이드 98 덴스", weight=305, pattern="18 : 20", headsize=98, length=27, balance=320, regDateInt=2021,
-           manufacturer="WILSON").save()
-    Racket(name="퓨어 드라이브", weight=300, pattern="16 : 19", headsize=100, length=27, balance=330, regDateInt=2019,
-           manufacturer="BABOLAT").save()
-    Racket(name="퓨어 에어로", weight=300, pattern="16 : 19", headsize=100, length=27, balance=330, regDateInt=2019,
-           manufacturer="BABOLAT").save()
-    Racket(name="스피드 프로", weight=310, pattern="18 : 20", headsize=100, length=27, balance=310, regDateInt=2020,
-           manufacturer="HEAD").save()
-    Racket(name="프레스티지 프로", weight=320, pattern="18 : 20", headsize=98, length=27, balance=310, regDateInt=2021,
-           manufacturer="HEAD").save()
-    Racket(name="그래비티 투어", weight=315, pattern="18 : 20", headsize=100, length=27, balance=310, regDateInt=2021,
-           manufacturer="HEAD").save()
-    Racket(name="프로스태프 97 RF", weight=340, pattern="16 : 19", headsize=97, length=27, balance=305, regDateInt=2021,
-           manufacturer="WILSON").save()
-    Racket(name="라디칼 프로", weight=315, pattern="16 : 19", headsize=98, length=27, balance=315, regDateInt=2020,
-           manufacturer="HEAD").save()
 
     RacketDetail(racket_id=1, adminReview="괜찮은 라켓입니다. 파워와 컨트롤을 둘다 골고루 가진 똑똑한 라켓입니다. "
                                           "약간의 무게감이 문제였지만 이번 버전에선 발란스가 일부 조정되었습니다."
--- racket/views.py
+++ racket/views.py
@@ -1,4 +1,5 @@
 from django.contrib import messages
+from django.core.paginator import Paginator
 from django.db.models import Avg, Func, FloatField, F
 from django.http import HttpRequest, request
 from django.shortcuts import render, redirect, get_object_or_404
@@ -11,6 +12,7 @@ from visitor.models import VisitorReview
 def racketMain(request: HttpRequest):
     getSearchKeyword = request.GET.get('searchKeyword', '')
     sort = request.GET.get('sort', '')
+    page = request.GET.get('page', '1')
 
     if getSearchKeyword:
         getRacket = Racket.objects.filter(name__icontains=getSearchKeyword).order_by('name')
@@ -27,6 +29,9 @@ def racketMain(request: HttpRequest):
         else:
             getRacket = Racket.objects.order_by('name')
             getAvgScore = VisitorReview.objects.filter(visitorRacket_id=1).aggregate(Avg('visitorScore'))
+
+    paginator = Paginator(getRacket, 10)  # 페이지당 10개씩 보여주기
+    getRacket = paginator.get_page(page)
 
     return render(request, "racket/racketMain.html", {'racketItems': getRacket,
                                                       'racketUserScore': getAvgScore})
