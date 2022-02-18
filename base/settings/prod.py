05c1954b786e9b4a8380f26139279578a237b5df
tlsrb0025 <tlsrb0025@gmail.com>
2022-02-18 12:41:24 +0900

    접속용데이터베이스접속정보수정6

--- base/settings/prod.py
+++ base/settings/prod.py
@@ -5,7 +5,8 @@ DEBUG = False
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'sbsst',
+        'NAME': 'snr',
+        'USER': 'sbsst',
         'PASSWORD': 'sbs123414',
         'HOST': '172.17.0.1',
         'PORT': '3306',
