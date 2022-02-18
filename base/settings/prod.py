e03c43cf14dff10c0013b09573ef3a49eca4de46
tlsrb0025 <tlsrb0025@gmail.com>
2022-02-18 12:31:17 +0900

    개발용데이터베이스접속정보수정

--- base/settings/prod.py
+++ base/settings/prod.py
@@ -5,15 +5,18 @@ DEBUG = False
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'snr', # 깃에 올릴거임. 근거리에서밖에 접속 못함. 사람륻이 알아도 접속 불가
-        'USER': 'tlsrb0025',
-        'PASSWORD': 'server123414',
-        'HOST': '172.17.0.1',
-        'PORT': '3306',
+        'NAME': 'snr',
+        'USER': 'snrst',
+        'PASSWORD': 'snr123414',
+        'HOST': '127.0.0.1',
+        'PORT': '3308',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            # 추가, 만약에 이 부분 때문에 오류가 난다면, 이 라인을 지우고 다시 시도해주세요.
+            # 추가, 만약에 이 부분 때문에 오류가 난다면 삭제할 것
             # MySQL Strict Mode 켜기
+            'charset': 'utf8mb4',
+            # 이모지 등록 가능
+            'use_unicode': True,
         }
     }
 }
