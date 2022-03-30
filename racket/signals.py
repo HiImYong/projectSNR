dc032966cc28ca4c9912e5a7cebf46f5e257de41
tlsrb0025 <tlsrb0025@gmail.com>
2022-03-30 22:08:52 +0900

    거ì삭제시평점리프레쉬

--- racket/signals.py
+++ racket/signals.py
@@ -1,5 +1,5 @@
 from django.db.models import Avg
-from django.db.models.signals import post_save, pre_save
+from django.db.models.signals import post_save, pre_save, pre_delete, post_delete
 from django.dispatch import receiver
 
 from racket.models import Racket
@@ -10,6 +10,14 @@ from racketReview.models import RacketReviewModel
 def on_post_product_save(sender, instance: RacketReviewModel, created: bool, raw: bool, using, update_fields, **kwargs):
     racket: Racket = instance.visitorRacket
 
+    getVisitorAvgScore = racket.racketreviewmodel_set.aggregate(Avg('visitorScore'))['visitorScore__avg']
+    racket.visitorAvgScore = getVisitorAvgScore
+    racket.save()
+
+
+@receiver(post_delete, sender=RacketReviewModel)
+def delete_review(sender, instance, **kwargs):
+    racket: Racket = instance.visitorRacket
     getVisitorAvgScore = racket.racketreviewmodel_set.aggregate(Avg('visitorScore'))['visitorScore__avg']
     racket.visitorAvgScore = getVisitorAvgScore
     racket.save()
