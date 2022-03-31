b27c94e178da7d00508a3ccafebe2966410e9b93
tlsrb0025 <tlsrb0025@gmail.com>
2022-03-31 12:03:10 +0900

    아이피수정

--- base/settings/settings.py
+++ base/settings/settings.py
@@ -97,7 +97,7 @@ DATABASES = {
         'USER': 'snrst',
         'PASSWORD': 'snr123414',
         'HOST': '172.17.0.1',
-        'PORT': '3308',
+        'PORT': '3306',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
             # 추가, 만약에 이 부분 때문에 오류가 난다면 삭제할 것
